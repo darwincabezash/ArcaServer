490396975f1c3102d2ec3f3d1d7773d48c083536
Darwin Cabezas <darwincabezash@gmail.com>
2021-12-13 11:50:57 -0500

    Correccion de funcionamiento Server

--- public/schemas/grupoS.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.GrupoS = void 0;
-const GrupoS = "\ntype Query{\n    grupo:[Grupo]   \n}\n\ntype Grupo{\n    _id:ID\n    tipo:String\n}\n\ninput GrupoInput{\n    tipo:String\n}\n\ntype Mutation{\n    crearGrupo(input:GrupoInput):Grupo\n    eliminarGrupo(_id:ID):Grupo\n    actualizarGrupo(_id:ID,input:GrupoInput):Grupo\n}\n";
-exports.GrupoS = GrupoS;
-//# sourceMappingURL=grupoS.js.map
--- public/schemas/user.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.User = void 0;
-const User = "\n\ntype Query{\n    user:[User]\n}\n\ntype User{\n    _id:ID\n    firstName:String\n    lastName:String\n    age:Int\n}\n\ninput UserInput{\n    firstName:String\n    lastName:String\n    age:Int\n}\n\n\ntype Mutation{\n    createUser(input:UserInput):User\n    deleteUser(_id:ID):User\n    updateUser(_id:ID,input:UserInput):User\n}\n";
-exports.User = User;
-//# sourceMappingURL=user.js.map
