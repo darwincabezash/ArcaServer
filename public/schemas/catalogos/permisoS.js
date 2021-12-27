8cdf76a27220853cccb8461cc15c78be4591ac3d
Darwin Cabezas <darwincabezash@gmail.com>
2021-12-27 10:44:34 -0500

    Corrigiendo error Cors 1

--- public/schemas/catalogos/permisoS.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.PermisoS = void 0;
-const PermisoS = "\ntype Query{\n    permiso:[Permiso]   \n}\n\ntype Permiso{\n    _id:ID\n    nombre:String\n    estado:Boolean\n}\n\ninput PermisoInput{\n    nombre:String\n    estado:Boolean\n}\n\ntype Mutation{\n    crearPermiso(input:PermisoInput):Permiso\n}\n";
-exports.PermisoS = PermisoS;
-//# sourceMappingURL=permisoS.js.map
--- public/schemas/catalogos/tipoProcesoS.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.TipoProcesoS = void 0;
-const TipoProcesoS = "\ntype Query{\n    tipoProceso:[TipoProceso]   \n}\n\ntype Mutation{\n    crearTipoProceso(input:TipoProcesoInput):TipoProceso\n    eliminarTipoProceso(_id:ID):TipoProceso\n    actualizarTipoProceso(_id:ID,input:TipoProcesoInput):TipoProceso\n}\n\ntype TipoProceso{\n    _id:ID\n    tipo:String\n}\n\ninput TipoProcesoInput{\n    tipo:String\n}\n";
-exports.TipoProcesoS = TipoProcesoS;
-//# sourceMappingURL=tipoProcesoS.js.map
